eb6994fa42c02568730cf118309353390e3636c1
Ahmed Idani <ahmed.idani@insat.ucar.tn>
2025-08-10 21:09:47 +0100

    Arrays & Hashing 1

--- Arrays_Hashing/easy/Valid_Anagram/valid_anagram.py
+++ Arrays_Hashing/easy/Valid_Anagram/valid_anagram.py
@@ -1,3 +1,4 @@
+from copy import copy
 class Solution:
     
     def isAnagram(self, s: str, t: str) -> bool:
@@ -7,18 +8,11 @@ class Solution:
             "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0,
             "y": 0, "z": 0
         }
-        t_dict = {
-            "a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0,
-            "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0,
-            "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0,
-            "y": 0, "z": 0
-        }
+        t_dict = copy(s_dict)
         for c in s:
             s_dict[c]+=1
         for c in t:
             t_dict[c]+=1
-        print("t_dict=", t_dict)
-        print("s_dict=", s_dict)
         return (True if s_dict == t_dict else False)
 
 
